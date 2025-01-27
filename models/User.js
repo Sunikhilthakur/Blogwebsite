614f5a85e767b0287e0d47e99652d264e80ca619
mac <mac@macs-MacBook-Pro.local>
2025-01-27 11:04:47 +0530

    Final

--- models/User.js
+++ models/User.js
@@ -1,23 +1,4 @@
-// // models/User.js
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema; 
 
-// const userSchema = new Schema({
-//   username: String,
-//   password: String,
-//   email: String,
-//   isAdmin: { type: Boolean, default: false },
-//   blog: [
-//     {
-//       type: Schema.Types.ObjectId,
-//       ref: "Blog"
-//     }
-//   ]
-// });
-
-// const User = mongoose.model('User', userSchema);
-// module.exports = User;
-// models/User.js
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
